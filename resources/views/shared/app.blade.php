b78da2fc5ccbdbd8366e6f4028ecf20fad335246
Rafael Juliano Ferreira <67478757+RafaelJuliano@users.noreply.github.com>
2022-01-19 21:17:47 -0300

    nav css finished

--- resources/views/shared/app.blade.php
+++ resources/views/shared/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Scripts -->
     
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('/js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -31,8 +31,8 @@
             @include('shared.header')
         </header>            
         <main>
-            @yield('nav')
-            @yield('content')
+            @include('shared.nav')
+            @yield('content')            
         </main>
     </div>
 </body>
---
+++ resources/views/shared/nav.blade.php
@@ -0,0 +1,60 @@
+<input class="close-check" type='checkbox' id='close-menu' hidden />
+
+<nav class="side-menu">    
+    <label class="open-menu menu-button" for="close-menu">
+        <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>
+    </label>
+
+    <label class="close-menu menu-button" for="close-menu">
+        <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>
+    </label>
+    
+    <ul class="menu-group">
+
+        <input class="cadastro-check" type='checkbox' id='cadastro' name='cadastro' hidden />
+        <li class="menu-item-main">
+            <a>
+                <label for='cadastro'>                    
+                    <span class="close-menu">Cadastros</span> 
+                    <svg class="open-menu tooltip-item" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18 14.45v6.55h-16v-12h6.743l1.978-2h-10.721v16h20v-10.573l-2 2.023zm1.473-10.615l1.707 1.707-9.281 9.378-2.23.472.512-2.169 9.292-9.388zm-.008-2.835l-11.104 11.216-1.361 5.784 5.898-1.248 11.103-11.218-4.536-4.534z"/></svg>   
+                    <span class="tooltip">Cadastros</span>             
+                </label>
+                
+            </a>
+        </li>
+            
+
+        <li class="menu-item-child">
+            <a href="#">
+                <span class="close-menu child-span">Produtos</span> 
+                <svg class="open-menu tooltip-item" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M11.5 23l-8.5-4.535v-3.953l5.4 3.122 3.1-3.406v8.772zm1-.001v-8.806l3.162 3.343 5.338-2.958v3.887l-8.5 4.534zm-10.339-10.125l-2.161-1.244 3-3.302-3-2.823 8.718-4.505 3.215 2.385 3.325-2.385 8.742 4.561-2.995 2.771 2.995 3.443-2.242 1.241v-.001l-5.903 3.27-3.348-3.541 7.416-3.962-7.922-4.372-7.923 4.372 7.422 3.937v.024l-3.297 3.622-5.203-3.008-.16-.092-.679-.393v.002z"/></svg>
+                <span class="tooltip">Produtos</span>
+            </a>
+            
+        </li>
+
+        <li class="menu-item-child">
+            <a href="">
+                <span class="close-menu child-span">Clientes</span>
+                <svg class="open-menu tooltip-item" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M7 16.488l1.526-.723c1.792-.81 2.851-.344 4.349.232 1.716.661 2.365.883 3.077 1.164 1.278.506.688 2.177-.592 1.838-.778-.206-2.812-.795-3.38-.931-.64-.154-.93.602-.323.818 1.106.393 2.663.79 3.494 1.007.831.218 1.295-.145 1.881-.611.906-.72 2.968-2.909 2.968-2.909.842-.799 1.991-.135 1.991.72 0 .23-.083.474-.276.707-2.328 2.793-3.06 3.642-4.568 5.226-.623.655-1.342.974-2.204.974-.442 0-.922-.084-1.443-.25-1.825-.581-4.172-1.313-6.5-1.6v-5.662zm-1 6.538h-4v-8h4v8zm1-7.869v-1.714c-.006-1.557.062-2.447 1.854-2.861 1.963-.453 4.315-.859 3.384-2.577-2.761-5.092-.787-7.979 2.177-7.979 2.907 0 4.93 2.78 2.177 7.979-.904 1.708 1.378 2.114 3.384 2.577 1.799.415 1.859 1.311 1.853 2.879 0 .13-.011 1.171 0 1.665-.483-.309-1.442-.552-2.187.106-.535.472-.568.504-1.783 1.629-1.75-.831-4.456-1.883-6.214-2.478-.896-.304-2.04-.308-2.962.075l-1.683.699z"/></svg>
+                <span class="tooltip">Clientes</span>
+            </a>
+        </li>  
+
+    </ul>
+
+    <ul class="menu-group">
+
+        <li class="menu-item-main">
+            <a>
+                <label  for='vendas'>                    
+                    <span class="close-menu">Vendas</span> 
+                    <svg class="open-menu tooltip-item" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M2 9.453v-9.453h9.352l10.648 10.625-3.794 3.794 1.849 4.733-12.34 4.848-5.715-14.547zm1.761 1.748l4.519 11.503 10.48-4.118-1.326-3.395-4.809 4.809-8.864-8.799zm-.761-10.201v8.036l9.622 9.552 7.963-7.962-9.647-9.626h-7.938zm12.25 8.293c-.415-.415-.865-.617-1.378-.617-.578 0-1.227.241-2.171.803-.682.411-1.118.585-1.456.585-.361 0-1.083-.409-.961-1.219.052-.345.25-.696.572-1.019.652-.652 1.544-.848 2.276-.107l.744-.744c-.476-.475-1.096-.792-1.761-.792-.566 0-1.125.228-1.663.677l-.626-.626-.698.699.653.652c-.569.826-.842 2.021.076 2.937 1.011 1.011 2.188.541 3.413-.232.6-.379 1.083-.563 1.475-.563.589.001 1.18.498 1.078 1.258-.052.386-.26.764-.621 1.122-.451.451-.904.679-1.347.679-.418 0-.747-.192-1.049-.462l-.739.739c.463.458 1.082.753 1.735.753.544 0 1.087-.201 1.612-.597l.54.538.697-.697-.52-.521c.743-.896 1.157-2.209.119-3.247zm-9.25-7.292c1.104 0 2 .896 2 2s-.896 2-2 2-2-.896-2-2 .896-2 2-2zm0 1c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1z"/></svg> 
+                    <span class="tooltip">Vendas</span>                                 
+                </label>
+            </a>
+        </li>
+        <input class="cadastro-check" type='checkbox' id='vendas' name='vendas' hidden />
+    </ul>    
+    
+</nav>
