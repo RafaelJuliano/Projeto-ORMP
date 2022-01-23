d2c8c34f8d36bf7597af52d0eaa917c25e926dbf
Rafael Juliano Ferreira <67478757+RafaelJuliano@users.noreply.github.com>
2022-01-23 13:43:24 -0300

    config - database

--- config/database.php
+++ config/database.php
@@ -66,17 +66,18 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $url['host'],
+            'port' => $url["port"],
+            'database' => ltrim($url["path"], "/"),
+            'username' => $url["user"],
+            'password' => $url['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
+      
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
